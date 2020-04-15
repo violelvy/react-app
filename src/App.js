68bbdec75ce17cc3050b0351c6b0b0ff259168a0
wangxiang <monicawoo_x@163.com>
2020-04-15 22:51:47 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-15 22:13:10
+ * @LastEditTime: 2020-04-15 22:23:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-app/src/App.js
+ */
+import React from 'react';
+function App() {
+  return (
+    <div >
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-15 22:13:10
+ * @LastEditTime: 2020-04-15 22:25:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-app/src/index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
